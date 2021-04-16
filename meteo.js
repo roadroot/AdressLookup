8a968217f5161fd04993d8d65e9b4d090525b012
LydiaBec <80390636+LydiaBec@users.noreply.github.com>
2021-04-16 19:38:13 +0200

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,4 +1,5 @@
 
+
 function fetch(name, postcode, limit, autocomplete, lon, lat, type, async = true) {
   let url = "";
   if (name) url += "?q=" + name;
@@ -29,16 +30,28 @@ function onQuery() {
     return;
   }
   new Promise(() => {
-    let result = fetch(name, postcode, limit, true, lon, lat, type, async = false).features;
+    result = fetch(name, postcode, limit, true, lon, lat, type, async = false).features;
     console.log(result)
-    let table = document.createElement("table");
+    table = document.createElement("table");
     domResult.appendChild(table)
     console.log(result)
     if(result.length == 0)
       alert('0 addresses found');
-    for (res in result) {
-      table.appendChild(createCouple("geometry", `${result[res].geometry.coordinates[1]},${result[res].geometry.coordinates[0]}`))
-    }
+
+    table.appendChild(createCouple("Properties", "Values"))
+    table.appendChild(createCouple("Geometry", `${result[res].geometry.coordinates[1]},${result[res].geometry.coordinates[0]}`))
+    table.appendChild(createCouple("City Code :", `${result[res].properties.citycode}`))
+    table.appendChild(createCouple("Context :", `${result[res].properties.context}`))
+    table.appendChild(createCouple("id : ",  `${result[res].properties.id}`))
+    table.appendChild(createCouple("Importance :", `${result[res].properties.importance}`))
+    table.appendChild(createCouple("label :", `${result[res].properties.label}`))
+    table.appendChild(createCouple("Name : ", `${result[res].properties.name}`))
+    table.appendChild(createCouple("Postcode :", `${result[res].properties.postcode}`))
+    table.appendChild(createCouple("Score :", `${result[res].properties.score}`))
+    table.appendChild(createCouple("Type : ", `${result[res].properties.type}`))
+    table.appendChild(createCouple("X :", `${result[res].properties.x}`))
+    table.appendChild(createCouple("Y :", `${result[res].properties.y}`))
+    document.getElementById("navigation").style.display = 'block'
   });
 }
 
@@ -55,4 +68,48 @@ function createCouple(name, value) {
   tr.appendChild(nm);
   tr.appendChild(val);
   return tr;
+}
+/*********************************************************** */
+var res = 0;
+var table = document.createElement("table");
+var result;
+
+
+
+
+function nextPage() {
+  res++;
+  if(res > result.length - 1 ){
+    res--;
+    alert("Derniere page !");
+  }else{
+    table.innerHTML = '';
+    affichageTableau(res);
+  }
+}
+
+function previousPage() {
+  if(res > 0){
+    res--;
+    table.innerHTML = '';
+    affichageTableau(res);
+  }else{
+    alert("1ere valeur affichée")
+  }
+}
+
+function affichageTableau(res){
+  table.appendChild(createCouple("Properties", "Values"))
+  table.appendChild(createCouple("Geometry", `${result[res].geometry.coordinates[1]},${result[res].geometry.coordinates[0]}`))
+  table.appendChild(createCouple("City Code :", `${result[res].properties.citycode}`))
+  table.appendChild(createCouple("Context :", `${result[res].properties.context}`))
+  table.appendChild(createCouple("id : ",  `${result[res].properties.id}`))
+  table.appendChild(createCouple("Importance :", `${result[res].properties.importance}`))
+  table.appendChild(createCouple("label :", `${result[res].properties.label}`))
+  table.appendChild(createCouple("Name : ", `${result[res].properties.name}`))
+  table.appendChild(createCouple("Postcode :", `${result[res].properties.postcode}`))
+  table.appendChild(createCouple("Score :", `${result[res].properties.score}`))
+  table.appendChild(createCouple("Type : ", `${result[res].properties.type}`))
+  table.appendChild(createCouple("X :", `${result[res].properties.x}`))
+  table.appendChild(createCouple("Y :", `${result[res].properties.y}`))
 }
