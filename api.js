c17c86f5fe984fad9bbb274ec9a81a2cd9e052e1
roadroot <mohamed.azikiou@gmail.com>
2021-04-16 14:20:39 +0200

    fc

--- api.js
+++ api.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 
-function getScheduleByID({tid, startHourRange = 8, endHourRange = 19, startDayRange = 0, endDayRangeIncluded = 6, path = "file.json"}) {
-    if(startHourRange == null) startHourRange = 8;
-    if(endHourRange == null) endHourRange = 19;
-    if(startDayRange == null) startHourRange = 0;
-    if(endDayRangeIncluded == null) endDayRangeIncluded = 6;
+function getScheduleByID({tid, startHourRange: shr = 8, endHourRangeIncluded: ehr = 19, startDayRange = "monday", endDayRangeIncluded = "sunday", path = "file.json"}) {
+    if(shr == null) shr = 8;
+    if(ehr == null) ehr = 19;
+    if(startDayRange == null) startDayRange = "monday";
+    if(endDayRangeIncluded == null) endDayRangeIncluded = "sunday";
     schedule = {};
     let data = fs.readFileSync(path);
-    let json = JSON.parse(data)
+    let json = JSON.parse(data);
+    let week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
     for (let course of json.courses) {
-        if (course.teacher == tid && course.hour >= startHourRange && course.hour <= endHourRange) {
+        let day = week.indexOf(course.day);
+        let sdr = week.indexOf(startDayRange);
+        let edr = week.indexOf(endDayRangeIncluded);
+        if (course.teacher == tid && sdr <= day && day <= edr && !(edr == day && course.hour > ehr || sdr == day && course.hour < shr)) {
             if (schedule[course.day] == null)
                 schedule[course.day] = {};
             schedule[course.day][course.hour] = { course: course.id, "duration": course.duration };
@@ -17,4 +21,4 @@ function getScheduleByID({tid, startHourRange = 8, endHourRange = 19, startDayRa
     }
     return schedule;
 }
-console.log(getScheduleByID({tid: 0, startHourRange: 13, endHourRange: 13}));
+console.log(getScheduleByID({tid: 1, startHourRange: 12, endHourRangeIncluded: 13, startDayRange: "saturday", endDayRangeIncluded: "saturday"}));
