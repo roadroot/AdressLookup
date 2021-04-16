d32bcf46fac1d506ab0c3c23c6e027ba1c24ea81
roadroot <mohamed.azikiou@gmail.com>
2021-04-16 06:10:42 +0200

    fc

--- api.js
+++ api.js
@@ -1,11 +1,15 @@
-import { readFileSync } from "fs";
+const fs = require("fs");
 
-function getScheduleByID(tid, path = "file.json") {
+function getScheduleByID({tid, startHourRange = 8, endHourRange = 19, startDayRange = 0, endDayRangeIncluded = 6, path = "file.json"}) {
+    if(startHourRange == null) startHourRange = 8;
+    if(endHourRange == null) endHourRange = 19;
+    if(startDayRange == null) startHourRange = 0;
+    if(endDayRangeIncluded == null) endDayRangeIncluded = 6;
     schedule = {};
-    let data = readFileSync(path);
+    let data = fs.readFileSync(path);
     let json = JSON.parse(data)
     for (let course of json.courses) {
-        if (course.teacher == tid) {
+        if (course.teacher == tid && course.hour >= startHourRange && course.hour <= endHourRange) {
             if (schedule[course.day] == null)
                 schedule[course.day] = {};
             schedule[course.day][course.hour] = { course: course.id, "duration": course.duration };
@@ -13,4 +17,4 @@ function getScheduleByID(tid, path = "file.json") {
     }
     return schedule;
 }
-console.log(getScheduleByID(0));
+console.log(getScheduleByID({tid: 0, startHourRange: 13, endHourRange: 13}));
